96952709c727d6d0e4028c14cd267c9ce737fd30
Duahstik <118763651+Duahstik@users.noreply.github.com>
2022-12-07 21:36:12 +0300

    Add files via upload

---
+++ creating a dynamic array in the constructor, destructor, classes, public methods/ConsoleApplication16/ConsoleApplication16.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+class test1
+{
+private:
+    int* data;
+public:
+    test1(int size)
+    {
+        this->data = new int[size];
+    }
+  
+    void zapolni(int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            data[i] = rand() % 100;
+        }
+    }
+    
+    void vike(int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << data[i] << "\t";
+        }
+    }
+
+    ~test1()
+    {
+        delete[]data;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    system("color 02");
+    int num = 5;
+    test1 g(num);
+    g.zapolni(num);
+    g.vike(num);
+}
